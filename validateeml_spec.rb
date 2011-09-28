93afb55a1a8198d19e93df656b590cbc8d617604
Sven Bohm <bohms@msu.edu>
2011-09-27 19:34:00 -0700

    initial commit

---
+++ validateeml_spec.rb
@@ -0,0 +1,19 @@
+require 'rspec'
+
+describe CheckEML do
+
+  describe 'validate eml' do
+    it 'should validate'
+  end
+
+  describe "creating a dataset" do
+    it 'should create a valid url'
+    it 'should parse the response'
+  end
+
+
+  describe 'gather a report' do
+    it 'should construct a valid url'
+    it 'should parse the response'
+  end
+end
