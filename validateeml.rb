b6b2f4575bc15413a9c86f452c05364144ff8e27
Sven Bohm <bohms@msu.edu>
2011-09-27 22:33:34 -0700

    removed some duplication

--- validateeml.rb
+++ validateeml.rb
@@ -18,22 +18,8 @@ class CheckEML
       check_dataset(url)
     end
 
-    print "\n"
-    p @errors
-  end
-
-  def created_dataset_in_nis(url)
-    begin
-      # use Request.execute because we want to disable the timeout.
-      response = RestClient::Request.execute(:method=>:post, 
-                                             :url =>'http://data.lternet.edu/data/eml?mode=evaluate', 
-                                             :payload => url, 
-                                             :timeout => -1)
-      pass_fail(response) 
-    rescue RestClient::Exception => e
-      @errors.push EMLCheckError.new(url, e.response.body)
-      print 'F'
-    end
+    print_errors
+    print_reporst
   end
 
   def check_dataset(url)
@@ -42,27 +28,28 @@ class CheckEML
     eml_scope, doc_id, version = doc.root.attributes['packageId'].value.split(/\./)
     data_entities =  doc.xpath('//dataTable/entityName').collect {|x| x.text }
 
-    if check_with_eml_validator(url)
-      if created_dataset_in_nis(url)
-        if have_any_data_entities_been_created?(eml_scope, doc_id, version)
-          read_nis_data_reports(data_entities, eml_scope, doc_id, version)
-        end
-      end
-    end
+    check_with_eml_validator(url)
+    created_dataset_in_nis(url)
+    have_any_data_entities_been_created?(eml_scope, doc_id, version)
+    read_nis_data_reports(data_entities, eml_scope, doc_id, version)
+  end
+
+
+  def created_dataset_in_nis(url)
+      # use Request.execute because we want to disable the timeout.
+      check_with RestClient::Request.execute(:method=>:post, 
+                                             :url =>'http://data.lternet.edu/data/eml?mode=evaluate', 
+                                             :payload => url, 
+                                             :timeout => -1)
   end
 
   def have_any_data_entities_been_created?(eml_scope, doc_id, version)
-    begin 
-      url = "http://data.lternet.edu/data/eml/NIS-#{eml_scope}/#{doc_id}/#{version}"
-      response = RestClient.get url
-      pass_fail(response)
-    rescue RestClient::Exception => e
-      fail_with_message(url, e.response.body)
-    end
+      check_with RestClient.get "http://data.lternet.edu/data/eml/NIS-#{eml_scope}/#{doc_id}/#{version}"
   end
 
   def read_nis_data_reports(data_entities, eml_scope, doc_id, version)
       data_entities.each do | entity |
+        check_with 
         begin
           url = "http://data.lternet.edu/data/eml/NIS-#{eml_scope}/#{doc_id}/#{version}/#{entity}/report"
           response = RestClient.get url
@@ -93,6 +80,26 @@ class CheckEML
     end
   end
 
+  def print_errors
+    p @errors
+  end
+
+  def print_reports
+    p @reports
+  end
+
+  private
+
+  def check_with
+    begin
+      response = yield 
+      pass_fail(response) 
+    rescue RestClient::Exception => e
+      @errors.push EMLCheckError.new(url, e.response.body)
+      print 'F'
+    end
+  end
+
   def fail_with_message(url, message) 
     print 'F'
     @errors.push(url, message)
@@ -106,6 +113,7 @@ class CheckEML
       print 'F'
     end
   end
+
 end
 
 EMLCheckError  = Struct.new(:url, :error)
